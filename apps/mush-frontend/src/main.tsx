15a2b3fb4b7e4371e9561e319cb1e31a4303ff92
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-08-22 23:59:15 +0300

    feat(frontend): update title and implement error boundary with routing

--- apps/mush-frontend/src/main.tsx
+++ apps/mush-frontend/src/main.tsx
@@ -1,14 +1,30 @@
+import '@/index.css';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
 
-import './index.css';
-import App from './App.tsx';
+import App from '@/App.tsx';
+import ErrorBoundary from '@/ErrorBoundary';
+
+/* eslint-disable react-refresh/only-export-components */
+const NotFound = () => {
+  const location = useLocation();
+  throw new Error(`404 Not Found: ${location.pathname}`);
+};
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <BrowserRouter>
+        <ErrorBoundary>
+          <Routes>
+            <Route path='/' element={<App />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </ErrorBoundary>
+      </BrowserRouter>
     </StrictMode>,
   );
 }
