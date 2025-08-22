f9b2b3186d47f9e0171ff560379ac69c167c8c25
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-08-22 13:04:57 +0300

    feat(mush-frontend): initialize project structure with Tailwind CSS and basic components

--- apps/mush-frontend/src/main.tsx
+++ apps/mush-frontend/src/main.tsx
@@ -1,22 +1,14 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './style.css';
-import typescriptLogo from '/typescript.svg';
-import { Header, Counter } from '@repo/ui';
 
-const App = () => (
-  <div>
-    <a href='https://vitejs.dev' target='_blank'>
-      <img src='/vite.svg' className='logo' alt='Vite logo' />
-    </a>
-    <a href='https://www.typescriptlang.org/' target='_blank'>
-      <img src={typescriptLogo} className='logo vanilla' alt='TypeScript logo' />
-    </a>
-    <Header title='Web' />
-    <div className='card'>
-      <Counter />
-    </div>
-  </div>
-);
+import './index.css';
+import App from './App.tsx';
 
-createRoot(document.getElementById('app')!).render(<App />);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
