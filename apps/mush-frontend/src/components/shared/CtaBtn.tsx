67c7a3c806352def88ca6dea8810a9ddc27aaeb9
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-08-24 23:17:07 +0300

    feat: replace Start button with CtaBtn component in ConsentDialog

---
+++ apps/mush-frontend/src/components/shared/CtaBtn.tsx
@@ -0,0 +1,28 @@
+import { forwardRef } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+interface CtaBtnProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+const CtaBtn = forwardRef<HTMLButtonElement, CtaBtnProps>(
+  ({ children, ...props }: CtaBtnProps, ref) => (
+    <Button
+      className={cn(
+        'dark:hover:bg-foreground rounded-full px-12 py-6 text-xl transition duration-200 hover:opacity-95 hover:shadow-xl focus:ring-2',
+        'hover:shadow-[0_20px_25px_-5px_rgba(0,0,0,0.1),0_10px_10px_-5px_rgba(0,0,0,0.04)]',
+        'dark:hover:shadow-[0_20px_25px_-5px_rgba(255,255,255,0.1),0_10px_10px_-5px_rgba(255,255,255,0.04)]',
+      )}
+      ref={ref}
+      {...props}
+    >
+      {children}
+    </Button>
+  ),
+);
+
+CtaBtn.displayName = 'CtaBtn';
+
+export default CtaBtn;
