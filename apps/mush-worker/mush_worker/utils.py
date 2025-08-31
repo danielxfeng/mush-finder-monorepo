41efff99009fc730a38197fa4634877388a59a8c
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-08-31 17:03:59 +0300

    Update image download URL to include cropping and quality parameters

--- apps/mush-worker/mush_worker/utils.py
+++ apps/mush-worker/mush_worker/utils.py
@@ -9,7 +9,7 @@ _async_client = httpx.AsyncClient(follow_redirects=True, timeout=10.0)
 
 
 async def image_download(url: AnyUrl) -> Image.Image | None:
-    img_url = str(url).replace("/upload/", "/upload/w_224,h_224/")  # auto resize
+    img_url = str(url).replace("/upload/", "/upload/w_224,h_224,c_fill,q_100/")  # auto resize
 
     try:
         img_file = await _async_client.get(img_url)
